bbd8a47bb8de2e9e2a04f5eb61b4d78b6da59920
makkob <maximkabig945@gmail.com>
2022-09-29 19:26:16 +0300

    hw3

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader';
@@ -7,9 +8,34 @@ import Modal from './Modal';
 import './App.module.css';
 
 export default function App() {
+    const [articles, setArticles] = useState([]);
+    let [error, setError] = useState(null);
+    let [loader, setLoader] = useState(false);
+
+    useEffect(() => {
+        setLoader(true);
+        axios
+            .get(
+                'https://pixabay.com/api/?q=cat&page=1&key=28517920-47e926602853ad98d512bf5fa&image_type=photo&orientation=horizontal&per_page=12',
+            )
+            .then(({ data }) => setArticles(data.hits))
+            .catch(error => {
+                return setError(error);
+            })
+            .finally(() => setLoader(false));
+    }, []);
+
     return (
         <>
             <Searchbar />
+            <ul>
+                {articles.map(({ id, previewURL }) => (
+                    <li key={id}>
+                        <img src={previewURL} />
+                    </li>
+                ))}
+            </ul>
+
             <ImageGallery />
             <Loader />
             <Button />
