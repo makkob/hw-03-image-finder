1d869add2826942757ff724577ca05a43aa2a83c
makkob <maximkabig945@gmail.com>
2022-10-02 16:34:48 +0300

    hw3f

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader';
 import Button from './Button';
 import Modal from './Modal';
+import fetchImagesWithQuery from '../services/services';
 import './App.module.css';
 
 export default function App() {
     const [items, setItems] = useState([]);
     let [error, setError] = useState(null);
     let [loader, setLoader] = useState(false);
+    let [searchQuery, setSearchQuery] = useState('');
+    let [page, setPage] = useState(1);
 
     useEffect(() => {
+        fetchImages();
+
+        // setLoader(true);
+        // axios
+        //     .get(
+        //         'https://pixabay.com/api/?q=car&page=1&key=28517920-47e926602853ad98d512bf5fa&image_type=photo&orientation=horizontal&per_page=12',
+        //     )
+        //     .then(({ data }) => setItems(data.hits))
+        //     .catch(error => {
+        //         return setError(error);
+        //     })
+        //     .finally(() => setLoader(false));
+    }, []);
+
+    let fetchImages = () => {
         setLoader(true);
-        axios
-            .get(
-                'https://pixabay.com/api/?q=car&page=1&key=28517920-47e926602853ad98d512bf5fa&image_type=photo&orientation=horizontal&per_page=12',
-            )
-            .then(({ data }) => setItems(data.hits))
+
+        fetchImagesWithQuery(searchQuery, page)
+            .then(items => setItems(items))
             .catch(error => {
                 return setError(error);
             })
             .finally(() => setLoader(false));
-    }, []);
+    };
 
     let onHandleSubmit = inputValue => {
-        console.log(inputValue);
+        setSearchQuery(inputValue);
+        fetchImages();
+        console.log('inputValue=', inputValue);
+        console.log('searchQuery =', searchQuery);
     };
 
     return (
---
+++ src/services/services.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_KEY = '28517920-47e926602853ad98d512bf5fa';
+
+function fetchImagesWithQuery(query, page = 1, perPage = 12) {
+    return axios(
+        `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`,
+    ).then(response => response.data.hits);
+}
+
+export default fetchImagesWithQuery;
